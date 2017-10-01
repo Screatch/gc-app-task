118e2d35faabef4f423059c9f82acf669d6b1560
Vitali Raikov <screatch@gmail.com>
2017-10-01 13:14:28 +0300

    Gemfile changes, and added heartbeat route for AWS ELB

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,12 @@
 class MainController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:index]
 
   def index
 
+  end
+
+  def heartbeat
+    # For AWS ELB
+    head :ok
   end
 end
